97abf5656d5fb6e8e88f38eed2f761b127fe8ac9
Welson Dutra <welsonpereira83@gmail.com>
2023-06-28 21:23:59 -0300

    identação

--- src/screen/Skills/styles.js
+++ src/screen/Skills/styles.js
@@ -3,16 +3,9 @@ import * as colors from '../../theme';
 
 export const Section = styled.section`
   display: flex;
-  /* align-items: center;
-  justify-content: center; */
 
   margin: 0;
   min-height: 100vh;
   padding: 7rem;
   background-color: ${colors.default.dark3};
-
-  /* @media (max-width: 999px) {
-    flex-direction: column;
-    justify-content: space-around;
-  } */
 `;
